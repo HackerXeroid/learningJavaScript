345708a0e92efba7cde7184e6c6de2535a8c9a56
Shivam Singhgit config --global user.name Shivam <hackerxeroid@outlook.comgit config --global user.email hackerxeroid@outlook.com>
2023-05-19 12:15:40 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+"use strict";
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+
+const openModal = function (e) {
+  // e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+// const ulEls = document.getElementsByTagName("li");
+const header = document.querySelector("header");
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+message.innerHTML = `We use cookies for improved functionality & analytics. <button class="btn btn--close-cookie">Got it!</button>`;
+message.style.backgroundColor = "#37383d";
+
+header.before(message);
+message.style.height = `${parseFloat(getComputedStyle(message).height) + 25}px`;
+
+document
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", () => message.parentNode.removeChild(message));
+
+const myEl = document.querySelector(".hello");
+
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
+btnScrollTo.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log(e.target);
+  section1.scrollIntoView({ behavior: "smooth" });
+});
